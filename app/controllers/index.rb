6523ab30b31e4650e7659b73857e747cac23a7a2
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-28 14:53:05 -0500

    working shortner

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,30 @@
 get '/' do
+  @errors
   # let user create new short URL, display a list of shortened URLs
+  erb :index
 end
 
 post '/urls' do
   # create a new Url
+  @url = Url.new(long_url: params[:long], click_count: 0)
+  if @url.save
+    p @url
+    erb :success_page
+  else
+    p @error
+
+    @error = @url.errors.messages[:long_url][0]
+    p @error
+    erb :index
+  end
 end
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirect to appropriate "long" URL
+  @url = Url.find_by_short_url(params[:short_url])
+  @url.click_count += 1
+  @url.save
+  redirect (@url.long_url)
 end
 
